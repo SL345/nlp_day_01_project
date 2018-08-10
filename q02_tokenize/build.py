08fc3f55b085b17557f10368e54b256ed5126498
SL345 <sherilyn345@gmail.com>
2018-08-10 11:54:26 +0000

    Done

--- q02_tokenize/build.py
+++ q02_tokenize/build.py
@@ -1,3 +1,4 @@
+# %load q02_tokenize/build.py
 # Default imports
 
 from nltk.tokenize import TreebankWordTokenizer
@@ -7,3 +8,11 @@ import pandas as pd
 from greyatomlib.nlp_day_01_project.q01_load_data.build import q01_load_data
 
 # Write your solution here:
+def q02_tokenize(path):
+    df,X_train,X_test,y_train,y_test = q01_load_data(path)
+    x_train_ser = pd.Series(X_train)
+    x_train_lower_case = x_train_ser.str.lower()
+    tokenizer = TreebankWordTokenizer()
+    variable = x_train_lower_case.apply(lambda row: tokenizer.tokenize(str(row)))
+    return variable
+
